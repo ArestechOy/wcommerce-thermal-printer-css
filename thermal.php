aca473e34c871c754ed699df87af84a68f1cd278
artoparikka81 <37731123+artoparikka81@users.noreply.github.com>
2022-06-20 16:27:45 +0300

    Create thermal.php

---
+++ thermal.php
@@ -0,0 +1,45 @@
+function example_address_free_receipt() {
+    ?>
+    <style>
+            #page {
+                font-size:14pt;
+                font: Georgia, "Times New Roman", Times, serif;
+                margin-left:0;
+                margin-right:0;
+                page-break-inside : avoid 
+            }
+            .content {
+                padding-left: 0%;
+                padding-right: 0%;
+                padding-top: 0;
+                padding-bottom: 0;
+                line-height: 1.3em;
+            }
+            .order-items .head-name, .order-items .product-name, .order-items .total-name {
+                width: 25%;
+                max-width: 90px;
+            }
+            .order-addresses {
+                margin-bottom: 20px;
+            }
+            .order-branding, .order-addresses, .order-info, .order-items, .order-notes, .order-thanks, .order-colophon {
+            margin-bottom: 0;
+            }
+            thead
+                {
+                    display: table-row-group;
+                }
+            @page{
+                margin-left: 0px;
+                margin-right: 0px;
+                margin-top: 0px;
+                margin-bottom: 0px;
+                thead
+                {
+                    display: table-row-group;
+                }
+            }
+            
+            
+            </style>
+    <?php
